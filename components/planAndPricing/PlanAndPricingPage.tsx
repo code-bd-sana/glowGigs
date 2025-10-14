cac07a0e9db661e0fa6f3a757db32fbd32450e46
Syed Rakib Hasan <bannah76769@gmail.com>
2025-10-14 17:49:13 +0600

    design price and plan page

---
+++ components/planAndPricing/PlanAndPricingPage.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Pricing from './Pricing'
+import UserFeedback from './UserFeedback'
+
+export default function PlanAndPricingPage() {
+  return (
+    <div>
+      
+      <Pricing/>
+      <UserFeedback/>
+    </div>
+  )
+}
