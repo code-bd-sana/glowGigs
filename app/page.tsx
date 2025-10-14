cd0a28772ff53eb93f87cc6f1be39e963795958b
Sanaullah Shakil <sanaullahshakil673@gmail.com>
2025-10-14 14:26:06 +0600

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -14,10 +14,10 @@ export default function Page() {
       <HomeBanner></HomeBanner>
       <FindYourPerfect />
       <OurMission />
-      <Testimonials/>
-      <Footer/>
       <ConnectingProfessionals/>
       <Community/>
+      <Testimonials/>
+      <Footer/>
     </div>
   );
 }
