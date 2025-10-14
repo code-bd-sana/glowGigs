15eca95d69aae1ce2d5823a1e3b4e4ef9a698021
AnkonChy <ankonchy1@gmail.com>
2025-10-14 10:06:32 +0600

    custom button hover color & bg changed

--- app/page.tsx
+++ app/page.tsx
@@ -10,37 +10,6 @@ export default function Page() {
     <div>
       <NavBar></NavBar>
       <HomeBanner></HomeBanner>
-      <h1 className="heading text-4xl">Welcome to Glow Gigs</h1>
-      <p className="text-lg">This paragraph uses Lato font by default.</p>
-      <Button text="Get Started" />
-      <SectionHeader
-        title="our Mission"
-        // subtitle="Explore Nature Safely"
-        description="We aim to provide a seamless platform for specialists to find rewarding opportunities and for companies to discover exceptional talent."
-        align="left"
-      />
-
-      <SectionHeader
-        title="Join Us Now"
-        description="Become a member and get exclusive content about eco-adventures."
-        align="center"
-      />
-      <SectionHeader
-        title="Connecting Professionals"
-        description="‘We bridge the gap becween health and beauty specialists and top employers."
-        // align="center"
-      />
-      <div>
-        <div>
-          <SectionHeader
-            title="Find your perfect fit"
-            description="GlowGigs is your online bridge to connect health and beauty specialists with top employers in the industry."
-            // align="center"
-          />
-          <Button text="" />
-        </div>
-        <div></div>
-      </div>
       <FindYourPerfect />
       <OurMission />
     </div>
--- components/shared/Button.tsx
+++ components/shared/Button.tsx
@@ -17,7 +17,7 @@ const Button: React.FC<ButtonProps> = ({
     <button
       type={type}
       onClick={onClick}
-      className={`px-4 py-2 md:px-14 md:py-4 text-black rounded-full my-2 lg:my-10 border border-black bg-transparent hover:bg-white hover:text-black transition ${className}`}
+      className={`px-4 py-2 md:px-14 md:py-4 text-black rounded-full my-2 lg:my-10 border border-black bg-transparent hover:bg-black hover:text-white transition ${className}`}
     >
       {text}
     </button>
