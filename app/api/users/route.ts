112b83442f2e8fede83d98dd89b651a553f5c76a
Syed Rakib Hasan <bannah76769@gmail.com>
2025-10-18 09:20:19 +0600

    solved type error in user route.js

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,8 +1,8 @@
 import { dbConnect } from "@/lib/dbConnect"
-import { NextResponse } from "next/server"
+import { NextRequest, NextResponse } from "next/server"
 import User from "./user.model";
 
-export const GET = async(req:Request)=>{
+export const GET = async(req:NextRequest)=>{
 
     try {
 
@@ -25,23 +25,22 @@ export const GET = async(req:Request)=>{
 
 
 
-export const POST = async(req: Request, res:Response) =>{
-    try {
+export const POST = async (req: NextRequest) => {
+  try {
+    await dbConnect();
 
+    const data = await req.json(); 
+    const newUser = new User(data);
+    const saved = await newUser.save();
 
-        const data = req.body;
-        const newUser = new User(data);
-        const saved = await newUser.save();
-        return NextResponse.json({
-            message:"Success",
-            data:saved
-        }, {status: 200})
-        
-    } catch (error) {
-        
-        return NextResponse.json({
-            message:"Something went wrong!",
-            error
-        }, {status: 500})
-    }
-}
+    return NextResponse.json(
+      { message: "Success", data: saved },
+      { status: 200 }
+    );
+  } catch (error) {
+    return NextResponse.json(
+      { message: "Something went wrong!", error },
+      { status: 500 }
+    );
+  }
+};
