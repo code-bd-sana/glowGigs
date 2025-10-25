062df26a8f4d0ed802ea7575183253d62245a3b7
KhalidTheCoder <khalidahsan2407@gmail.com>
2025-10-25 16:45:30 +0600

    updated admin dashboard

--- components/(dashboard)/AdminDashboard.tsx
+++ components/(dashboard)/AdminDashboard.tsx
@@ -3,49 +3,34 @@
 import OverviewCard from "../JobSeeker/OverviewCard";
 import RecentApplications from "../JobSeeker/RecentApplications";
 import { ApplicationChart } from "../JobSeeker/ApplicationChart";
-
-
-import AdminCard from './admin/AdminCard'
-import AdminChart from './admin/AdminChart'
-import AdminPieChart from './admin/AdminPieChart'
-import React from 'react'
-
+import AdminCard from "./admin/AdminCard";
+import AdminChart from "./admin/AdminChart";
+import AdminPieChart from "./admin/AdminPieChart";
+import React from "react";
 
 export default function AdminDashboard() {
-
   return (
     <div>
       <OverviewCard></OverviewCard>
       <div className="flex gap-5 mt-10">
-       <div className="flex-1">
-         <RecentApplications></RecentApplications>
-       </div>
-      <div className="flex-1">
-        <ApplicationChart></ApplicationChart>
+        <div className="flex-1">
+          <RecentApplications></RecentApplications>
+        </div>
+        <div className="flex-1">
+          <ApplicationChart></ApplicationChart>
+        </div>
       </div>
-      </div>
-      <OverviewCard></OverviewCard>
-      <div className="flex gap-5 mt-10">
-       <div className="flex-1">
-         <RecentApplications></RecentApplications>
-       </div>
-      <div className="flex-1">
-        <ApplicationChart></ApplicationChart>
-      </div>
-      </div>
-     <AdminCard/> 
+      <AdminCard />
       {/*joy bangla  */}
 
-<div className='lg:flex mt-8 gap-8 overflow-hidden'>
-<div className='lg:w-[40%]'>
-      <AdminPieChart/>
-</div>
-     <div className='flex-1'>
-        <AdminChart/>
-     </div>
-   
-</div>
-
+      <div className="lg:flex mt-8 gap-8 overflow-hidden">
+        <div className="lg:w-[40%]">
+          <AdminPieChart />
+        </div>
+        <div className="flex-1">
+          <AdminChart />
+        </div>
+      </div>
     </div>
   );
 }
